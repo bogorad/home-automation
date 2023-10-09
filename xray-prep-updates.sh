380678af97cdf350ed516b2a94c073dfaf799780
Eugene Bogorad <bogorad@gmail.com>
2023-10-09 22:14:03 +0000

    upd

--- xray-prep-updates.sh
+++ xray-prep-updates.sh
@@ -19,6 +19,12 @@ curl "https://api.bgpview.io/asn/2906/prefixes" | jq ".data.ipv4_prefixes[].pref
 # get hulu
 curl "https://api.bgpview.io/asn/23286/prefixes" | jq ".data.ipv4_prefixes[].prefix" | sed 's|"||g'>hulu.txt
 
+# get youtube
+#curl "https://api.bgpview.io/asn/1026/prefixes" | jq ".data.ipv4_prefixes[].prefix" | sed 's|"||g'>youtube.txt
+#curl "https://api.bgpview.io/asn/36040/prefixes" | jq ".data.ipv4_prefixes[].prefix" | sed 's|"||g'>>youtube.txt
+#curl "https://api.bgpview.io/asn/36561/prefixes" | jq ".data.ipv4_prefixes[].prefix" | sed 's|"||g'>>youtube.txt
+#curl "https://api.bgpview.io/asn/43515/prefixes" | jq ".data.ipv4_prefixes[].prefix" | sed 's|"||g'>>youtube.txt
+
 # complie to output/dat/mygeo.dat
 go run ./
 
@@ -44,5 +50,6 @@ cd ~/git/home-automation
 scp mysites.dat mygeo.dat root@r5s.bruc:/usr/share/xray/
 git add mysites.dat
 git add mygeo.dat
+git add xray-prep-updates.sh
 git commit -m "upd"
 git push
